0896bb32b25a6307db1a109f24da577e3d342ffa
Orongto <orongto@gmail.com>
2021-01-28 20:24:39 +0100

    Added initial project structure.

---
+++ src/Somi.Desktop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Somi.Desktop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
